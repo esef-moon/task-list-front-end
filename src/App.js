4a2acaa12fb16411e11c9dcf59d37fe86adb4724
Susi <susi.franco32@gmail.com>
2023-06-20 14:18:29 -0700

    uplifted completed state to app, and created delete function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,37 @@ const TASKS = [
 ];
 
 const App = () => {
+  //setting my "tasks" default state as TASKS array
+  const [tasks, setTasks] = useState(TASKS);
+  // created updatedTask function, that then creates a copy of task list
+  // matches task to be updated with id, and return an updated lis of tasks, including task changed. 
+  const updateTask = (taskId, completedStatus) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        const updatedStatusTask = {... task};
+        updatedStatusTask.isComplete = !completedStatus;
+        return updatedStatusTask;
+      } return {...task};
+    });
+    setTasks(newTasks);
+  };
+ 
+  const deleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);     
+  };
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        {/* settign prop name update task to function we created on line 19, also make sure your original data prop is set to the state data*/}
+        <div>
+          <TaskList tasks={tasks} updateTask={updateTask} deleteTask={deleteTask} />
+          </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, updateTask, deleteTask }) => {
+  // toggle button to set iscomplete class of html element to completed-True or blank-False
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => updateTask(id, isComplete)}
       >
         {title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" onClick={() => deleteTask(id)}>x</button>
     </li>
   );
 };
@@ -24,6 +24,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateTask: PropTypes.func.isRequired, 
+  deleteTask: PropTypes.func.isRequired
 };
 
 export default Task;
